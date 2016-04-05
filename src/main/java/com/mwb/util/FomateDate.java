1f298d205a179f8e864ea66d125765fe513be7cc
mwb <13029727767@163.com>
2016-04-05 16:38:07 +0800

    New changelist

--- src/main/java/com/mwb/util/FomateDate.java
+++ src/main/java/com/mwb/util/FomateDate.java
@@ -1,6 +1,7 @@
 package com.mwb.util;
 
 import java.text.ParseException;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -9,11 +10,36 @@ import java.util.Date;
  * 格式化日期
  */
 public class FomateDate {
-    public static String fomattoString(Date date){
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        String str=sdf.format(date);
+    /**
+     * 日期转换成字符串
+     * @param date
+     * @return str
+     */
+    public static String DateToStr(Date date) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String str = format.format(date);
         return str;
     }
+    /**
+     * 字符串转换成日期
+     * @param str
+     * @return date
+     */
+    public static Date StrToDate(String str) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = null;
+        try {
+            date = format.parse(str);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("日期转字符串：" + FomateDate.DateToStr(date));
+        System.out.println("字符串转日期：" + FomateDate.StrToDate(FomateDate.DateToStr(date)));
 
+    }
 
 }
