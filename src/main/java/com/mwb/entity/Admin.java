0f521a36f65e3564a17a0670793b046b5e9ce64c
mwb <13029727767@163.com>
2016-04-08 23:02:17 +0800

    admin

---
+++ src/main/java/com/mwb/entity/Admin.java
@@ -0,0 +1,105 @@
+package com.mwb.entity;
+
+import com.mwb.util.FomateDate;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2016/4/5 0005.
+ * 管理员
+ */
+public class Admin {
+    private  int id;
+    private  String name;
+    private  String  email;
+    private  String  password;
+    private  String  tel;
+    private  String  lastdatestr;
+    private Date lastdate;
+    private  int grade;
+    //级别
+    public Admin() {
+    }
+    public Admin(String email, int grade, String name, String password) {
+        this.email = email;
+        this.grade = grade;
+        this.name = name;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "admin{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getLastdatestr() {
+        return lastdatestr;
+    }
+
+    public void setLastdatestr(String lastdatestr) {
+        this.lastdatestr = lastdatestr;
+    }
+
+    public Date getLastdate() {
+        return lastdate;
+    }
+
+    public void setLastdate(Date lastdate) {
+        this.lastdate = lastdate;
+        this.lastdatestr = FomateDate.DateToStr(lastdate);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
